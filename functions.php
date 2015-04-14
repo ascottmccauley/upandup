6d17f6d58df65ce7724771d42b55334acc62cc52
ascottmccauley <scott@ascottmccauley.com>
2015-04-14 11:21:53 -0400

    formatting

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ $shortcodes = scandir(get_stylesheet_directory()  . '/includes/shortcodes');
 $shortcodes = array_slice($shortcodes, 2);
 
 foreach ( $shortcodes as $file ) {
-	if( !$filepath = locate_template( 'includes/shortcodes/' . $file ) ) {
+	if ( ! $filepath = locate_template( 'includes/shortcodes/' . $file ) ) {
 		trigger_error( sprintf( __( 'Error locating %s for inclusion', 'groundup' ), $file ), E_USER_ERROR );
 	}
 	require_once $filepath;
@@ -24,15 +24,15 @@ $post_types = scandir(get_stylesheet_directory()  . '/includes/post-types');
 $post_types = array_slice($post_types, 2);
 
 foreach ( $post_types as $file ) {
-	if( !$filepath = locate_template( 'includes/post-types/' . $file ) ) {
+	if ( ! $filepath = locate_template( 'includes/post-types/' . $file ) ) {
 		trigger_error( sprintf( __( 'Error locating %s for inclusion', 'groundup' ), $file ), E_USER_ERROR );
 	}
 	require_once $filepath;
 }
 
 // WooCommerce
-if(class_exists('Woocommerce')) {
-	add_theme_support('woocommerce');
+if ( class_exists( 'Woocommerce' ) ) {
+	add_theme_support( 'woocommerce' );
 	require_once('woocommerce/woo-functions.php');
 }
 
@@ -50,8 +50,8 @@ add_action( 'wp_footer', 'upandup_wrapper_end' );
 // Top Bar walker
 // Creates a new walker for the `primary` menu to follow the default styling for Zurb Foundation's top bar
 class Groundup_Topbar_Walker extends Walker_Nav_Menu {
-	function check_current($classes) {
-		return preg_match('/(current[-_])|active|dropdown/', $classes);
+	function check_current( $classes ) {
+		return preg_match( '/(current[-_])|active|dropdown/', $classes );
 	}
 	
 	function start_lvl(&$output, $depth = 0, $args = array()) {
@@ -60,27 +60,27 @@ class Groundup_Topbar_Walker extends Walker_Nav_Menu {
 	
 	function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
 		$item_html = '';
-		parent::start_el($item_html, $item, $depth, $args);
+		parent::start_el( $item_html, $item, $depth, $args );
 	
 		if (stristr($item_html, 'li class="divider')) {
-			$item_html = preg_replace('/<a[^>]*>.*?<\/a>/iU', '', $item_html);    
-		}elseif (stristr($item_html, 'li class="nav-header')) {
-			$item_html = preg_replace('/<a[^>]*>(.*)<\/a>/iU', '$1', $item_html);
+			$item_html = preg_replace( '/<a[^>]*>.*?<\/a>/iU', '', $item_html );    
+		}elseif ( stristr( $item_html, 'li class="nav-header' ) ) {
+			$item_html = preg_replace( '/<a[^>]*>(.*)<\/a>/iU', '$1', $item_html );
 		}   
 		$output .= $item_html;
 	}
 	
-	function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output) {
-		$element->is_dropdown = !empty($children_elements[$element->ID]);
+	function display_element( $element, &$children_elements, $max_depth, $depth = 0, $args, &$output ) {
+		$element->is_dropdown = ! empty( $children_elements[$element->ID] );
 	
-		if ($element->is_dropdown) {
-			if ($depth === 0) {
+		if ( $element->is_dropdown ) {
+			if ( $depth === 0 ) {
 				$element->classes[] = 'has-dropdown';
-			}elseif ($depth === 1) {
+			}elseif ( $depth === 1 ) {
 				$element->classes[] = 'has-dropdown';
 			}
 		}
-		parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
+		parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
 	}
 }
 
@@ -88,35 +88,35 @@ class Groundup_Topbar_Walker extends Walker_Nav_Menu {
 // Creates a new walker for the `mobile` menu to follow the default styling for Zurb Foundation's off-canvas menu
 class Groundup_Offcanvas_Walker extends Walker_Nav_Menu {
 	function check_current($classes) {
-		return preg_match('/(current[-_])|active|dropdown/', $classes);
+		return preg_match( '/(current[-_])|active|dropdown/', $classes );
 	}
 	
 	function start_lvl(&$output, $depth = 0, $args = array()) {
 		$output .= "\n<ul class='right-submenu'>\n<li class='back'><a href='#'>Back</a></li>\n";
 	}
 	
-	function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
+	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
 		$item_html = '';
-		parent::start_el($item_html, $item, $depth, $args);
+		parent::start_el( $item_html, $item, $depth, $args );
 	
 		if (stristr($item_html, 'li class="divider')) {
-			$item_html = preg_replace('/<a[^>]*>.*?<\/a>/iU', '', $item_html);    
+			$item_html = preg_replace( '/<a[^>]*>.*?<\/a>/iU', '', $item_html );    
 		}elseif (stristr($item_html, 'li class="nav-header')) {
-			$item_html = preg_replace('/<a[^>]*>(.*)<\/a>/iU', '$1', $item_html);
+			$item_html = preg_replace( '/<a[^>]*>(.*)<\/a>/iU', '$1', $item_html );
 		}   
 		$output .= $item_html;
 	}
 	
-	function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output) {
-		$element->is_dropdown = !empty($children_elements[$element->ID]);
+	function display_element( $element, &$children_elements, $max_depth, $depth = 0, $args, &$output ) {
+		$element->is_dropdown = ! empty( $children_elements[$element->ID] );
 	
-		if ($element->is_dropdown) {
-			if ($depth === 0) {
+		if ( $element->is_dropdown ) {
+			if ( $depth === 0 ) {
 				$element->classes[] = 'has-submenu';
-			}elseif ($depth === 1) {
+			}elseif ( $depth === 1 ) {
 				$element->classes[] = 'has-submenu';
 			}
 		}
-		parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
+		parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
 	}
 }
