f907c31b2e1e08af50dceb56e413945f55de915a
ascottmccauley <scott@ascottmccauley.com>
2015-04-21 13:09:25 -0400

    Reincorporated Off Canvas Menus

--- functions.php
+++ functions.php
@@ -45,11 +45,11 @@ add_action( 'groundup_inside_body', 'upandup_wrapper_start' );
 function upandup_wrapper_end() {
 	echo '</div></div>';
 }
-add_action( 'wp_footer', 'upandup_wrapper_end' );
+add_action( 'wp_footer', 'upandup_wrapper_end', 999 );
 
 // Top Bar walker
 // Creates a new walker for the `primary` menu to follow the default styling for Zurb Foundation's top bar
-class Groundup_Topbar_Walker extends Walker_Nav_Menu {
+class Upandup_Topbar_Walker extends Walker_Nav_Menu {
 	function check_current( $classes ) {
 		return preg_match( '/(current[-_])|active|dropdown/', $classes );
 	}
@@ -86,40 +86,63 @@ class Groundup_Topbar_Walker extends Walker_Nav_Menu {
 
 // Off Canvas walker
 // Creates a new walker for the `mobile` menu to follow the default styling for Zurb Foundation's off-canvas menu
-class Groundup_Offcanvas_Walker extends Walker_Nav_Menu {
+class Upandup_Offcanvas_Walker extends Walker_Nav_Menu {
 	function check_current($classes) {
-		return preg_match( '/(current[-_])|active|dropdown/', $classes );
+		return preg_match('/(current[-_])|active|dropdown/', $classes);
 	}
 	
 	function start_lvl(&$output, $depth = 0, $args = array()) {
 		$output .= "\n<ul class='right-submenu'>\n<li class='back'><a href='#'>Back</a></li>\n";
 	}
 	
-	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+	function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
 		$item_html = '';
-		parent::start_el( $item_html, $item, $depth, $args );
+		parent::start_el($item_html, $item, $depth, $args);
 	
 		if (stristr($item_html, 'li class="divider')) {
-			$item_html = preg_replace( '/<a[^>]*>.*?<\/a>/iU', '', $item_html );    
+			$item_html = preg_replace('/<a[^>]*>.*?<\/a>/iU', '', $item_html);    
 		}elseif (stristr($item_html, 'li class="nav-header')) {
-			$item_html = preg_replace( '/<a[^>]*>(.*)<\/a>/iU', '$1', $item_html );
+			$item_html = preg_replace('/<a[^>]*>(.*)<\/a>/iU', '$1', $item_html);
 		}   
 		$output .= $item_html;
 	}
 	
-	function display_element( $element, &$children_elements, $max_depth, $depth = 0, $args, &$output ) {
-		$element->is_dropdown = ! empty( $children_elements[$element->ID] );
+	function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output) {
+		$element->is_dropdown = !empty($children_elements[$element->ID]);
 	
-		if ( $element->is_dropdown ) {
-			if ( $depth === 0 ) {
+		if ($element->is_dropdown) {
+			if ($depth === 0) {
 				$element->classes[] = 'has-submenu';
-			}elseif ( $depth === 1 ) {
+			}elseif ($depth === 1) {
 				$element->classes[] = 'has-submenu';
 			}
 		}
-		parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+		parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
 	}
 }
+
+// Add offcanvas menu to footer
+function upandup_offcanvas_footer_menu () {
+	echo '<a class="exit-off-canvas"></a>';
+	echo '<aside class="right-off-canvas-menu">';
+	
+	$locations = get_nav_menu_locations();
+	$menu_object = get_term( $locations['mobile'], 'nav_menu' );
+	if ( $menu_object->count != 0 ) {
+		wp_nav_menu( array(
+			'theme_location' => 'mobile',
+			'menu_class' => 'off-canvas-list',
+			'walker'=> new Upandup_Offcanvas_Walker,
+		) );
+	} else {
+		wp_nav_menu( array(
+			'theme_location' => 'primary',
+			'menu_class' => 'off-canvas-list',
+			'walker'=> new Upandup_Offcanvas_Walker,
+		) );
+	}
+}
+add_action( 'wp_footer', 'upandup_offcanvas_footer_menu' );
 
 // Remove default page_title for pages
 function upandup_page_title( $page_title ) {
--- templates/nav.php
+++ templates/nav.php
@@ -6,17 +6,49 @@
  */
 ?>
 <?php // Desktop Menu
-if ( wp_is_mobile() && has_nav_menu( 'mobile' ) ) {
-	wp_nav_menu( array( 'theme_location' => 'mobile', 'container' => 'nav', 'walker'=> new Groundup_Offcanvas_Walker ) );
-} else if ( has_nav_menu( 'primary' ) ) { ?>
-	<nav id="primary_navigation" class="top-bar" role="navigation" data-topbar>
-		<?php if ( !is_front_page() && !groundup_is_new_user() ) { ?>
-			<ul class="title-area">
-				<li class="name"><h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1></li>
-			</ul>
+if ( ! wp_is_mobile() ) { ?>
+	<div class="sticky hide-for-small">
+		<nav id="primary_navigation" class="top-bar" role="navigation" data-topbar>
+			<?php // Add Company Name to Menubar if logo is not being shown
+			if ( ! is_front_page() && ! groundup_is_new_user() ) { ?>
+				<ul class="title-area">
+					<li class="name"><h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1></li>
+				</ul>
+			<?php } ?>
+			<section class="top-bar-section">
+				<?php wp_nav_menu( array(
+					'theme_location' => 'primary',
+					'container' => false,
+					'menu_class' => 'right',
+					'walker'=> new Upandup_Topbar_Walker,
+				) ); ?>
+			</section>
+		</nav>
+		<?php // See if secondary_menu has any items
+		$locations = get_nav_menu_locations();
+		$menu_object = get_term( $locations['secondary'], 'nav_menu' );
+		if ( $menu_object->count != 0 ) { ?>
+			<nav class="second-bar" role="navigation" data-topbar>
+				<section class="top-bar-section">
+					<?php wp_nav_menu( array(
+						'theme_location' => 'secondary_menu',
+						'menu_class' => 'right',
+						'walker'=> new Upandup_Topbar_Walker,
+					) ); ?>
+				</section>
+			</nav>
 		<?php } ?>
-		<section class="top-bar-section">
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false, 'menu_class' => 'right', 'walker'=> new Groundup_Topbar_Walker ) ); ?>
-		</section>
-	</nav>
-<?php }
+	</div>
+<?php } ?>
+
+<nav class="tab-bar show-for-small">
+	<section class="left-small">
+		<a class="left-off-canvas-toggle menu-button"><span></span></a>
+	</section>
+	<section class="middle tab-bar-section">
+		<h1 class="title"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+	</section>
+	<section class="right-small">
+		<a class="right-off-canvas-toggle menu-button"><span></span></a>
+	</section>
+</nav>
