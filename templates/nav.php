393262417d572ea6c073df0cefb9168f9794fcde
ascottmccauley <scott@ascottmccauley.com>
2015-05-06 08:22:31 -0400

    Remove logo from top-bar

--- templates/nav.php
+++ templates/nav.php
@@ -9,12 +9,6 @@
 if ( ! wp_is_mobile() ) { ?>
 	<div class="sticky hide-for-small">
 		<nav id="primary_navigation" class="top-bar" role="navigation" data-topbar>
-			<?php // Add Company Name to Menubar if logo is not being shown
-			if ( ! is_front_page() && ! groundup_is_new_user() ) { ?>
-				<ul class="title-area">
-					<li class="name"><h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1></li>
-				</ul>
-			<?php } ?>
 			<section class="top-bar-section">
 				<?php wp_nav_menu( array(
 					'theme_location' => 'primary',
