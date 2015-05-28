e34dca15974af614ff05ff3b7775daf223564563
ascottmccauley <scott@ascottmccauley.com>
2015-05-28 13:49:36 -0400

    bugfix

--- header.php
+++ header.php
@@ -21,7 +21,7 @@ if ( is_front_page() || groundup_is_new_user() ) {
 	</figure>
 	<h1 class="brand"><a href="<?php echo home_url(); ?>/"><?php bloginfo( 'name' ); ?></a></h1>
 	<?php if(get_bloginfo( 'description' ) != '') { ?>
-		<h2 class="description"><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'about' ) ) ); ?>"><?php echo get_bloginfo( 'description' ); ?></a></h2>
+		<h2 class="description"><a href="<?php echo esc_url( get_permalink( get_page_by_path( 'about' ) ) ); ?>" target="_self"><?php echo get_bloginfo( 'description' ); ?></a></h2>
 	<?php } ?>
 </header>
 
