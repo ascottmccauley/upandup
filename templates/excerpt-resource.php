f468be4e81309fe5ea8a515c9915571896badaa5
Scott McCauley <scottmccauley@SuzySmall.local>
2015-06-04 09:38:32 -0400

    Reorganize template

--- templates/excerpt-resource.php
+++ templates/excerpt-resource.php
@@ -11,18 +11,18 @@ $resourceID = get_post_meta( $post->ID, '_resource_media', true );
 $resourceURL = wp_get_attachment_url( $resourceID );
 $resourceType = get_post_mime_type( $resourceID );
 
-$thumbnail = get_the_post_thumbnail( $post->ID, 'small' );
+$thumbnail = get_the_post_thumbnail( $post->ID, 'medium' );
 if ( $thumbnail != '' ) { ?>
 	<article <?php post_class(); ?>>
 		<section class="entry-content">
-			<header>
-				<h5 class="entry-title">
-					<a href="<?php echo $resourceURL; ?>" rel="alternate" title="<?php the_title_attribute(); ?>" class="bookmark" target="_blank" type="<?php echo $resourceType; ?>"><?php the_title(); ?></a>
-				</h5>
-			</header>
-			<aside class="entry-thumbnail">
-				<?php echo $thumbnail; ?>
-			</aside>
+			<a href="<?php echo $resourceURL; ?>" rel="alternate" title="<?php the_title_attribute(); ?>" class="bookmark" target="_blank" type="<?php echo $resourceType; ?>">
+				<figure>
+					<?php echo $thumbnail; ?>
+				</figure>
+				<figcaption>
+					<?php the_title(); ?>
+				</figcaption>
+			</a>
 		</section>
 	</article>
 <?php } else { ?>
