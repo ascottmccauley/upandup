6e85663b4b5027982b833936adcd405e00e28e1a
Scott McCauley <scottmccauley@SuzySmall.local>
2015-06-15 11:15:04 -0400

    MSRP change

--- woocommerce/single-product/price.php
+++ woocommerce/single-product/price.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $product; ?>
 <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-	<h5 class="price"><?php echo $product->get_price_html(); ?></h5>
+	<h5 class="price"><small>MSRP: </small><?php echo $product->get_price_html(); ?></h5>
 	<meta itemprop="price" content="<?php echo $product->get_price(); ?>" />
 	<meta itemprop="priceCurrency" content="<?php echo get_woocommerce_currency(); ?>" />
 	<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
