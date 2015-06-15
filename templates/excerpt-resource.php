f9706c3836c66bfe42de5734484cd80521190279
Scott McCauley <scottmccauley@SuzySmall.local>
2015-06-15 11:11:47 -0400

    Change excerpts to <li> for block-grid

--- templates/excerpt-resource.php
+++ templates/excerpt-resource.php
@@ -13,7 +13,7 @@ $resourceType = get_post_mime_type( $resourceID );
 
 $thumbnail = get_the_post_thumbnail( $post->ID, 'medium' );
 if ( $thumbnail != '' ) { ?>
-	<article <?php post_class(); ?>>
+	<li <?php post_class( 'unstyled' ); ?>>
 		<section class="entry-content">
 			<a href="<?php echo $resourceURL; ?>" rel="alternate" title="<?php the_title_attribute(); ?>" class="bookmark" target="_blank" type="<?php echo $resourceType; ?>">
 				<figure>
@@ -24,7 +24,7 @@ if ( $thumbnail != '' ) { ?>
 				</figcaption>
 			</a>
 		</section>
-	</article>
+	</li>
 <?php } else { ?>
 	<a href="<?php echo $resourceURL; ?>" rel="alternate" title="<?php the_title_attribute(); ?>" class="bookmark" target="_blank" type="<?php echo $resourceType; ?>"><h5 class="entry-title"><i class="icon-file-text"></i> <?php the_title(); ?></h5></a>
 <?php } ?>
