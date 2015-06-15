56a50f29cf08429a00ed1dc3dd258a9ca1d6a340
Scott McCauley <scottmccauley@SuzySmall.local>
2015-06-15 11:10:54 -0400

    Eliminate small-logo

--- header.php
+++ header.php
@@ -5,18 +5,10 @@
  */
 ?>
 <?php get_template_part( 'head' ); ?>
-<?php // Homepage or First Visit
-if ( is_front_page() || groundup_is_new_user() ) { 
-	$class = ' large';
-	$logo =  get_stylesheet_directory_uri() . '/assets/img/logo.png';
-} else {
-	$class = '';
-	$logo = get_stylesheet_directory_uri() . '/assets/img/logo-small.png';
-} ?>
 <header id="header" role="banner" class="hide-for-small">
-	<figure class="logo<?php echo $class; ?>" role="logo">
+	<figure class="logo" role="logo">
 		<a href="<?php echo get_home_url(); ?>/">
-			<img src="<?php echo $logo; ?>">
+			<img src="<?php echo get_stylesheet_directory_uri() . '/assets/img/logo.png'; ?>">
 		</a>
 	</figure>
 	<h1 class="brand"><a href="<?php echo home_url(); ?>/"><?php bloginfo( 'name' ); ?></a></h1>
