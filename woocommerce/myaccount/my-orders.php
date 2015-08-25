773ed6c41b399e4bdeff18f47b686164760b5b0f
Scott McCauley <scottmccauley@SuzySmall.local>
2015-08-25 10:29:15 -0400

    Remove email templates and use filters instead

--- woocommerce/myaccount/my-orders.php
+++ woocommerce/myaccount/my-orders.php
@@ -22,74 +22,74 @@ $customer_orders = get_posts( apply_filters( 'woocommerce_my_account_my_orders_q
 ) ) );
 
 if ( $customer_orders ) : ?>
+	<div class="medium-6 columns">
+		<h2><?php echo apply_filters( 'woocommerce_my_account_my_orders_title', __( 'Recent Orders', 'woocommerce' ) ); ?></h2>
 
-	<h2><?php echo apply_filters( 'woocommerce_my_account_my_orders_title', __( 'Recent Orders', 'woocommerce' ) ); ?></h2>
+		<table class="shop_table shop_table_responsive my_account_orders">
 
-	<table class="shop_table shop_table_responsive my_account_orders">
+			<thead>
+				<tr>
+					<th class="order-number"><span class="nobr"><?php _e( 'Order', 'woocommerce' ); ?></span></th>
+					<th class="order-date"><span class="nobr"><?php _e( 'Date', 'woocommerce' ); ?></span></th>
+					<!-- <th class="order-status"><span class="nobr"><?php _e( 'Status', 'woocommerce' ); ?></span></th> -->
+					<th class="order-actions">&nbsp;</th>
+				</tr>
+			</thead>
 
-		<thead>
-			<tr>
-				<th class="order-number"><span class="nobr"><?php _e( 'Order', 'woocommerce' ); ?></span></th>
-				<th class="order-date"><span class="nobr"><?php _e( 'Date', 'woocommerce' ); ?></span></th>
-				<!-- <th class="order-status"><span class="nobr"><?php _e( 'Status', 'woocommerce' ); ?></span></th> -->
-				<th class="order-actions">&nbsp;</th>
-			</tr>
-		</thead>
+			<tbody><?php
+				foreach ( $customer_orders as $customer_order ) {
+					$order = wc_get_order( $customer_order );
+					$order->populate( $customer_order );
+					$item_count = $order->get_item_count();
 
-		<tbody><?php
-			foreach ( $customer_orders as $customer_order ) {
-				$order = wc_get_order( $customer_order );
-				$order->populate( $customer_order );
-				$item_count = $order->get_item_count();
+					?><tr class="order">
+						<td class="order-number" data-title="<?php _e( 'Order Number', 'woocommerce' ); ?>">
+							<a href="<?php echo esc_url( $order->get_view_order_url() ); ?>">
+								#<?php echo $order->get_order_number(); ?>
+							</a>
+						</td>
+						<td class="order-date" data-title="<?php _e( 'Date', 'woocommerce' ); ?>">
+							<time datetime="<?php echo date( 'Y-m-d', strtotime( $order->order_date ) ); ?>" title="<?php echo esc_attr( strtotime( $order->order_date ) ); ?>"><?php echo date_i18n( get_option( 'date_format' ), strtotime( $order->order_date ) ); ?></time>
+						</td>
+						<!-- <td class="order-status" data-title="<?php _e( 'Status', 'woocommerce' ); ?>" style="text-align:left; white-space:nowrap;">
+							<?php echo wc_get_order_status_name( $order->get_status() ); ?>
+						</td> -->
+						<td class="order-actions">
+							<?php
+								$actions = array();
 
-				?><tr class="order">
-					<td class="order-number" data-title="<?php _e( 'Order Number', 'woocommerce' ); ?>">
-						<a href="<?php echo esc_url( $order->get_view_order_url() ); ?>">
-							#<?php echo $order->get_order_number(); ?>
-						</a>
-					</td>
-					<td class="order-date" data-title="<?php _e( 'Date', 'woocommerce' ); ?>">
-						<time datetime="<?php echo date( 'Y-m-d', strtotime( $order->order_date ) ); ?>" title="<?php echo esc_attr( strtotime( $order->order_date ) ); ?>"><?php echo date_i18n( get_option( 'date_format' ), strtotime( $order->order_date ) ); ?></time>
-					</td>
-					<!-- <td class="order-status" data-title="<?php _e( 'Status', 'woocommerce' ); ?>" style="text-align:left; white-space:nowrap;">
-						<?php echo wc_get_order_status_name( $order->get_status() ); ?>
-					</td> -->
-					<td class="order-actions">
-						<?php
-							$actions = array();
-
-							if ( in_array( $order->get_status(), apply_filters( 'woocommerce_valid_order_statuses_for_payment', array( 'pending', 'failed' ), $order ) ) ) {
-								$actions['pay'] = array(
-									'url'  => $order->get_checkout_payment_url(),
-									'name' => __( 'Pay', 'woocommerce' )
-								);
-							}
-
-							if ( in_array( $order->get_status(), apply_filters( 'woocommerce_valid_order_statuses_for_cancel', array( 'pending', 'failed' ), $order ) ) ) {
-								$actions['cancel'] = array(
-									'url'  => $order->get_cancel_order_url( wc_get_page_permalink( 'myaccount' ) ),
-									'name' => __( 'Cancel', 'woocommerce' )
-								);
-							}
-
-							$actions['view'] = array(
-								'url'  => $order->get_view_order_url(),
-								'name' => __( 'View', 'woocommerce' )
-							);
-
-							$actions = apply_filters( 'woocommerce_my_account_my_orders_actions', $actions, $order );
-
-							if ( $actions ) {
-								foreach ( $actions as $key => $action ) {
-									echo '<a href="' . esc_url( $action['url'] ) . '" class="button small ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
+								if ( in_array( $order->get_status(), apply_filters( 'woocommerce_valid_order_statuses_for_payment', array( 'pending', 'failed' ), $order ) ) ) {
+									$actions['pay'] = array(
+										'url'  => $order->get_checkout_payment_url(),
+										'name' => __( 'Pay', 'woocommerce' )
+									);
 								}
-							}
-						?>
-					</td>
-				</tr><?php
-			}
-		?></tbody>
 
-	</table>
+								if ( in_array( $order->get_status(), apply_filters( 'woocommerce_valid_order_statuses_for_cancel', array( 'pending', 'failed' ), $order ) ) ) {
+									$actions['cancel'] = array(
+										'url'  => $order->get_cancel_order_url( wc_get_page_permalink( 'myaccount' ) ),
+										'name' => __( 'Cancel', 'woocommerce' )
+									);
+								}
 
+								$actions['view'] = array(
+									'url'  => $order->get_view_order_url(),
+									'name' => __( 'View', 'woocommerce' )
+								);
+
+								$actions = apply_filters( 'woocommerce_my_account_my_orders_actions', $actions, $order );
+
+								if ( $actions ) {
+									foreach ( $actions as $key => $action ) {
+										echo '<a href="' . esc_url( $action['url'] ) . '" class="button small ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
+									}
+								}
+							?>
+						</td>
+					</tr><?php
+				}
+			?></tbody>
+
+		</table>
+	</div>
 <?php endif; ?>
